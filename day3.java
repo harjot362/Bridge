416b3eba6bab0c022660982e77a6f83498bb1ab7
harjot <harjot362.be22@chitkara.edu.in>
2025-06-25 15:11:20 +0530

    basicAdd

---
+++ day3.java
@@ -0,0 +1,16 @@
+class day3{
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+        System.out.println("Sum of a and b is " + c);
+
+        int d = a * b;
+        System.out.println("Product of a and b is " + d);
+
+        int e = a / b;
+        System.out.println("Division of a by b is " + e);
+    }
+}
